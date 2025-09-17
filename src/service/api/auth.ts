dfa06b6ced6bfe6e531eddd393d2bbdc5a1ad8e4
winter100 <100taeng@gmail.com>
2025-09-17 19:34:13 +0900

    chore: 라이브러리 및 api 정리

--- src/service/api/auth.ts
+++ src/service/api/auth.ts
@@ -1,22 +1,53 @@
-import { SignInType, SignUpType } from "@/lib/validators/auth";
+import { SignInType, SignupPayloadType } from "@/lib/validators/auth";
+import { SignInResponseType } from "@/types/sign-type";
 
 const basicUrl = "http://localhost:8080";
 
-export const signIn = async (data: SignInType): Promise<string> => {
-  const response = await fetch(`${basicUrl}/auth/signin`, {
-    headers: { "Content-Type": "application/json" },
-    method: "POST",
-    body: JSON.stringify(data),
-  });
+export const signIn = async (data: SignInType): Promise<SignInResponseType> => {
+  try {
+    const response = await fetch(`${basicUrl}/auth/signin`, {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(data),
+    });
 
-  return response.json();
+    if (!response.ok) {
+      throw await response.json();
+    }
+
+    return response.json();
+  } catch (e) {
+    throw e;
+  }
 };
 
-export const signUp = async (data: SignUpType) => {
-  const response = await fetch(`${basicUrl}/auth/signup`, {
-    method: "POST",
-    body: JSON.stringify(data),
-  });
+export const signUp = async (data: SignupPayloadType) => {
+  try {
+    const response = await fetch(`${basicUrl}/auth/signup`, {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify(data),
+    });
 
-  return response.json();
+    if (!response.ok) {
+      throw await response.json();
+    }
+
+    return response.json();
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const checkEmail = async (email: string) => {
+  try {
+    const response = await fetch(`${basicUrl}/auth/check?email=${email}`, {
+      headers: { "Content-Type": "application/json" },
+      method: "GET",
+    });
+
+    return response.json();
+  } catch (e) {
+    throw e;
+  }
 };
